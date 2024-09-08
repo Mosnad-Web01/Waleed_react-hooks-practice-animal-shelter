df143a3a3182100a8cc25bf64bc4a523d053bbe6
waleed7333 <waleed733150@gmail.com>
2024-09-08 09:14:19 +0300

    update js files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const onChangeType = (type) => {
+    setFilters({ ...filters, type });
+  };
+
+  const onFindPetsClick = () => {
+    const url =
+      filters.type === "all"
+        ? "http://localhost:3001/pets"
+        : `http://localhost:3001/pets?type=${filters.type}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setPets(data));
+  };
+
+  const onAdoptPet = (id) => {
+    const updatedPets = pets.map((pet) =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +36,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={onChangeType}
+              onFindPetsClick={onFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
